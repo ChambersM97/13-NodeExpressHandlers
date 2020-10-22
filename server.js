acd997829cabf70563f19c240f72aab98d12a368
Chambers Mansour <chambersmansour97@gmail.com>
2020-10-22 12:12:04 -0500

    added json and git ignore

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+//allows us to use mysql in our js
+var mysql = require('mysql');
+
 //allows us to use express in our js
-var express = require('express')
+var express = require('express');
 
 //allows us to use handlebars
+var expressHandlebar = require('express-handlebars');
